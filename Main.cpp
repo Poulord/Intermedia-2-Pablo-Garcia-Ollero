f326ea7e3e3e5e4efb090e04834ee37722a985a5
Pablo <pablogo@msmk.university>
2024-11-24 21:10:20 +0100

    primer commit

---
+++ Main.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // Incluye la biblioteca para entrada y salida
+
+int main() {
+    std::cout << "Prueba uno" << std::endl; // Imprime "Hola, mundo!" en la consola
+    return 0; // Indica que el programa terminó correctamente
+}
+
